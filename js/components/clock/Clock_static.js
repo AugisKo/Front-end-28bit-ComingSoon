b9e67bf18413211f8f3bf59de8cf338b576a4024
Eugenijus <augisk@gmail.com>
2021-05-21 19:32:46 +0300

    Clock automation

--- js/components/clock/Clock_static.js
+++ js/components/clock/Clock_static.js
@@ -12,6 +12,7 @@ class Clock {
         if (!this.isValidSelector()) {
             return false;
         }
+
         this.render();
     }
 
@@ -45,41 +46,9 @@ class Clock {
         return updatedTime;
 
     }
-    calcDeadline() {
-        const dabartinisLaikas = new Date();
-        const einamiejiMetai = dabartinisLaikas.getFullYear();
-
-        const numanomaGimtadienioData = einamiejiMetai + '-' + this.targetDate;
-        const numanomasLaikas = new Date(numanomaGimtadienioData);
-
-        const dabartinesMilisekundes = dabartinisLaikas.getTime();
-        let numanomosMilisekundes = numanomasLaikas.getTime();
-
-        if (dabartinesMilisekundes > numanomosMilisekundes) {
-            numanomaGimtadienioData = (einamiejiMetai + 1) + '-' + this.targetDate;
-            numanomasLaikas = new Date(numanomaDimtadienioData);
-            numanomosMilisekundes = numanomasLaikas.getTime();
-        }
-
-        const likusiosMilisekundes = numanomosMilisekundes - dabartinesMilisekundes;
-        let likusiosSekundes = Math.floor(likusiosMilisekundes / 1000);
-        
-        const dienos = Math.floor(likusiosSekundes / 60 / 60 / 24);
-        likusiosSekundes -= dienos * 60 * 60 * 24;
-        
-        const valandos = Math.floor(likusiosSekundes / 60 / 60);
-        likusiosSekundes -= valandos * 60 * 60;
-
-        const minutes = Math.floor(likusiosSekundes / 60);
-        likusiosSekundes -= minutes * 60;
-
-        console.log(dienos, valandos, minutes, likusiosSekundes)
-        return [dienos, valandos, minutes, likusiosSekundes];
-        
-    }
 
     render() {
-        const timeValues = this.formatTime(this.calcDeadline());
+        const timeValues = this.formatTime([2, 9, 37, 39]);
         const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
         let HTML = '';
 
@@ -93,6 +62,18 @@ class Clock {
         this.DOM.innerHTML = HTML;
     }
 }
+//function Clock(selector){
+//    const DOM = document.querySelector(selector);
+//    const timeValues = [432, 9 ,37, 39];
+//    const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
+//    let HTML = '';
+//    for (let i = 0; i < timeValues.length; i++) {
+//        HTML+= `<div class="time">
+//                    <div class="value">${timeValues[i]}</div>
+//                    <div class="label">${labelValues[i]}</div>
+//                </div>`;      
+//    }
+//    DOM.innerHTML = HTML;
+//}
 
-    export {Clock}
-   
+export {Clock}
