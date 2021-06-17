089e13d40cd6487f20fc9468d326ed2def87d933
AugisKo <82829963+AugisKo@users.noreply.github.com>
2021-06-17 17:26:59 +0300

    Update socials.js

--- js/components/socials/socials.js
+++ js/components/socials/socials.js
@@ -1,17 +1,17 @@
 function socials(selector, data) {
-    //validation
+    // validation
 
-    //logic
+    // logic
     const DOM = document.querySelector(selector);
     let HTML = '';
-        for (const social of data) {
-            console.log(social);
-            HTML += `<a href="${social.href}" 
-                        target="_blank" 
-                        class="fa fa-${social.icon}"></a>`;
-        }
 
-    //result return
+    for (const social of data) {
+        HTML += `<a href="${social.href}"
+                    target="_blank"
+                    class="fa fa-${social.icon}"></a>`;
+    }
+
+    // result return
     DOM.innerHTML = HTML;
 }
 
