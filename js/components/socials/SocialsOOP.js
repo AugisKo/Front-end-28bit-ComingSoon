88e6f3ad709c27d94f3104b60a64fb9797c7a65b
Eugenijus <augisk@gmail.com>
2021-06-26 10:34:34 +0300

    socials OOP

---
+++ js/components/socials/SocialsOOP.js
@@ -0,0 +1,50 @@
+class Socialsoop {
+  constructor(selector, data) {
+    this.selector = selector;
+    this.data = data;
+
+    this.DOM = null;
+
+    this.init();
+  }
+  init() {
+    /*  if (!this.isValidSelector() || !this.isValidData()) {
+      cosole.error('ERROR: netraejo pirmines patkros');
+      return false;
+    }*/
+
+    this.DOM = document.querySelector(this.selector);
+
+    /* if (!this.DOM) {
+      console.error('ERROR: nerastas elementas, pagal duota selector');
+      return false;
+    }*/
+    this.render();
+  }
+  /*
+  isValidSelector() {
+    if (typeof this.selector !== 'string' || this.selector === '') {
+      return false;
+    }
+    return true;
+  }
+  isValidData() {
+    if (!Array.isArray(this.data) || this.data.length === 0) {
+      return false;
+    }
+    return true;
+  }*/
+  render() {
+    let HTML = '';
+
+    for (const social of data) {
+      HTML += `<a href="${social.href}"
+                    target="_blank"
+                    class="social fa fa-${social.icon}"></a>`;
+    }
+
+    this.DOM.innerHTML += HTML;
+  }
+}
+
+export { Socialsoop };
--- js/home.js
+++ js/home.js
@@ -1,12 +1,14 @@
 // IMPORT
 import { Clock } from './components/clock/Clock.js';
 import { Progressbar } from './components/progress-bar/ProgressBar.js';
-import { socials } from './components/socials/socials.js';
+// import { socials } from './components/socials/socials.js';
+import { Socialsoop } from './components/socials/SocialsOOP.js';
 import { clockData } from './data/clockData.js';
 import { progressBarData } from './data/progressBarData.js';
 import { socialsData } from './data/socialsData.js';
 
 // EXECUTION
 new Clock('#clock_1', clockData);
-socials('footer .socials', socialsData);
+// socials('footer .socials', socialsData);
+new Socialsoop('.socials', socialsData);
 new Progressbar('.left-column', progressBarData);
