e71fa39120e0b75bcd0507fbd636553feea24530
Eugenijus <augisk@gmail.com>
2021-06-26 13:03:28 +0300

    forms validation

---
+++ js/components/form/Form.js
@@ -0,0 +1,103 @@
+class Form {
+  constructor(selector) {
+    this.selector = selector;
+
+    this.DOM = null;
+    this.allInputsDOM = [];
+    this.submitButtonDOM = null;
+
+    this.init();
+  }
+  init() {
+    // patikrinti, ar validus selektorius
+    //jei bent vienas is ju nevalidus, tai baigiame darba
+    if (!this.isValidSelector()) {
+      cosole.error('ERROR: nepraejo pirmines patkros');
+      return false;
+    }
+
+    //susirasti DOM elementa
+    this.DOM = document.querySelector(this.selector);
+
+    //jei ne, baigiam darba
+    if (!this.DOM) {
+      console.error('ERROR: Elementa not found');
+      return false;
+    }
+
+    //susirasti visus formos laukus: input, textarea
+    //susirasti formos submit mygtuka
+    this.allInputsDOM = this.DOM.querySelectorAll('input, textarea');
+    this.submitButtonDOM = this.DOM.querySelector('button');
+
+    //uzregistruojame mygtuko paspaudimo ivyki
+    this.addEvents();
+  }
+  //patikrinti, ar validus selector
+  isValidSelector() {
+    if (typeof this.selector !== 'string' || this.selector === '') {
+      return false;
+    }
+    return true;
+  }
+  isValidEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+    //console.error('ERROR: Email address has wrong format');
+    return true;
+  }
+
+  isValidName(Name) {
+    if (!this.isValidMessage(Name)) {
+      console.error('ERROR: Name has to be a text');
+      return false;
+    }
+    if (Name.length < 2) {
+      return false;
+    }
+    if (Name[0].toLowerCase() === Name[0]) {
+      return false;
+    }
+    if (Name.slice(1).toLowerCase() !== Name.slice(1)) {
+      return false;
+    }
+    return true;
+  }
+
+  isValidMessage(message) {
+    if (typeof message !== 'string' || message === '') {
+      console.error('ERROR: Message has to be a text');
+      return false;
+    }
+    return true;
+  }
+
+  addEvents() {
+    this.submitButtonDOM.add.EventListener('click', (e) => {
+      e.preventDefault();
+      let allGood = true;
+      for (let element of this.allInputsDOM) {
+        const validationRule = element.dataset.validation;
+        if (validationRule === 'email') {
+          this.isValidEmail(element.value);
+          if (this.isValidEmail(element.value) === false) {
+            allGood === false;
+            break;
+          }
+        }
+      }
+      console.log('All Good?', allGood);
+    });
+
+    // submit mygtumo paspaudimo metu reikia isjungti default veikima
+
+    //issitraukti is visu formos lauku informacija
+    //eiti per visus laukus ir atpazinus informacijos tipa atlikti tos informacijos validacija
+
+    //jei patikrinus visus laukus nerasta nei vienos klaidos, tai "siunciam pranesima"
+    // jei patikrinus rasta bent viena klaida, tai parodome visus klaidos pranesimus (kol kas, viskas pateikiama i console )
+  }
+}
+
+export { Form };
--- js/home.js
+++ js/home.js
@@ -6,9 +6,12 @@ import { Socialsoop } from './components/socials/SocialsOOP.js';
 import { clockData } from './data/clockData.js';
 import { progressBarData } from './data/progressBarData.js';
 import { socialsData } from './data/socialsData.js';
+import { Form } from './components/form/Form.js';
 
 // EXECUTION
 new Clock('#clock_1', clockData);
 // socials('footer .socials', socialsData);
 new Socialsoop('footer .socials', socialsData);
 new Progressbar('.left-column', progressBarData);
+new Form('.hero form');
+new Form('main form');
